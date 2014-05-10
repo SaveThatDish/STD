cb0a2f383e3be97c3851fa38a27401ca6d415328
vishaalprasad <vishaal.prasad@gmail.com>
2014-05-10 13:21:09 -0700

    Modified Dish.java; Read the added comments.

--- SaveThatDish/src/com/example/savethatdish/Dish.java
+++ SaveThatDish/src/com/example/savethatdish/Dish.java
@@ -3,30 +3,60 @@ package com.example.savethatdish;
 import com.parse.ParseObject;
 
 
-
 public class Dish {
 
    private String name;
    private String restaurant;
    ParseObject dish;
 
-   public Dish(String dishName, String restaurantName) {
+   //we need to store the location data, not restaurant name. location implicitly has res. name as well
+   public Dish(String dishName, String restaurantName) { //what info is the location stored as... string? 
       this.name = dishName;
       this.restaurant = restaurantName;
       
-      dish = new ParseObject("Dish");
-   }
+      //if dish doesn't exist in parse
+        //addToParse(); //check return type/error handling. what do we do if it fails?
+      //else
+        //do nothing?
+      
+      //should we create a ParseQuery object? We'll need it for allLikes and onBucketList only, 
+      //so maybe only create the object in the methods that actually need it?
+      
+         }
 
-   // delete local copy, connect to parse?
-   public void removeDish() {
+   /*
+    * Creating the dish in the constructor doesn't make sense: what if the dish exists already and we're 
+    * only interested in creating a local var for the sake of convenience. Instead create a method that does that.
+    * 
+    * @Return: boolean to check for success? 
+    * OtheR: Error handling should be done within this method. Abstract that inf 
+    */
+   
+   public boolean addToParse() {
+	   dish = new ParseObject("Dish"); 
+	   //Associated info to add: restaurant name, location. Anything else?
+	   return true; //need to do error handling before returning
+   }
+   
+   /*
+    * Purpose: delete local copy, connect to parse?
+    * @Return: Boolean value indicating whether remove was successful
+    * Other: Error Handling
+    */
+   
+   public boolean removeDish() {
+	   return true;
    }
 
    /* 
     * @param: rating
+    * @return: Boolean value letting caller know if it was successful.
+    * Other: Error handling with Parse should be done in this method. Use that
+    * info to return the proper Boolean
     */
-   public void rateDish(int rating) // integer or boolean param?
+   public boolean rateDish(int rating) // integer or boolean param?
    {
-
+	   return true;
    }
 
    /* 
@@ -51,7 +81,10 @@ public class Dish {
     *  	    returns false if already on bucketlist
     */
    public boolean addBucketlist(String userName) {
-      return false;
+      if (onBucketlist(userName))
+        return false;
+      //else add to Parse
+      return true;
    }
 
 }
